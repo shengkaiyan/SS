e220b58f9252e588f500312830d4f1c57d98cf8e
sky <shengkaiyan@gmail.com>
2017-06-12 22:03:41 +0800

    开始加入google map.

---
+++ Shuji/Shuji/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shuji
+//
+//  Created by sky on 2017/6/12.
+//  Copyright © 2017年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shuji/Shuji/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shuji
+//
+//  Created by sky on 2017/6/12.
+//  Copyright © 2017年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
